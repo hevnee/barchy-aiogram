8bca290a2497901202817edbd05d19af0fca4292
hevnee <hevneebelous@gmail.com>
2025-06-29 23:46:03 +0200

    Update DELETE_PYCACHE.bat

--- DELETE_PYCACHE.bat
+++ DELETE_PYCACHE.bat
@@ -1,4 +1,5 @@
 @echo off
-rmdir /s /q bot\configs\__pycache__
+rmdir /s /q bot\__pycache__
 rmdir /s /q bot\services\__pycache__
 rmdir /s /q bot\utils\__pycache__
+rmdir /s /q bot\handlers\__pycache__
